658fc0b10861653fd5f31bac14bb0c851c282fba
LearningLab <ll@LearningLabs-MacBook-Pro-019.local>
2018-02-09 10:31:59 -0500

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,22 @@ router.get('/', function(req, res, next) {
   })
 });
 
+router.get('/stephanie', function(req, res, next){
+
+  var links = [
+  {text: "Google",
+  url:"https://www.google.com/"},
+  {text:"Facebook",
+  url: "https://www.facebook.com/"},
+  {text: "New York Times",
+  url:"https://www.nytimes.com/"}]
+
+  var theResult = links[2].url + ", " + links[1].text
+
+
+
+
+  res.render('steph_view', {title: 'Steph Page', data: links})
+})
+
 module.exports = router;
